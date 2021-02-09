bdd786fda4ebd5885daec0670f65830797fd1360
milena kurtz <milenatorino@aol.com>
2021-02-09 11:56:20 -0500

    unlike button commit

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,26 +3,41 @@ import { Link } from "react-router-dom";
 import "./Menu.css";
 import { withAsyncAction } from "../../redux/HOCs";
 
+
+
 class Menu extends React.Component {
+   
+
   handleLogout = event => {
     event.preventDefault();
     this.props.logout();
   };
 
+  
+  
   render() {
     return (
       <div className="Menu">
-        <h1>DEV TALK</h1>
+        <h1> TECH TALK</h1>
         {this.props.isAuthenticated && (
           <div id="menu-links">
             <Link to="/" onClick={this.handleLogout}>
               Logout
+            </Link>  
+
+            <div id="menu-links">
+            <Link to ="/"onClick={this.handleUsername}>
+            MockUser2_Team3
             </Link>
+
+           </div>
           </div>
         )}
       </div>
     );
+
   }
+
 }
 
 export default withAsyncAction("auth", "logout")(Menu);
